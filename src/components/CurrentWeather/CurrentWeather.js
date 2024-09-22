b3123cbc196143f0d363ab4e366f73f8d624111e
Kinshuk Rastogi <kinshuk.rastogi@technossus.com>
2024-09-23 02:49:56 +0530

    responsive-fix

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -7,9 +7,9 @@ const CurrentWeather = ({ data, queryInfo }) => {
 let description = data.current.weather[0].description;
 let capitalizedDescription = description.charAt(0).toUpperCase() + description.slice(1);
   return (
-    <div className='CurrentWeather col-xs-4'>
-      <div className='city-header'>{queryInfo.city}</div>
-      <div className='weather-info-block'>
+    <div className='CurrentWeather row'>
+      <div className='city-header col-m-4'>{queryInfo.city}</div>
+      <div className='weather-info-block col-m-8'>
         <h2>{queryInfo.unit == 'Celsius' ? (data.current.temp - 273).toPrecision(4) : ((data.current.temp - 273) * 9 / 5 + 32).toPrecision(4)} °{queryInfo.unit == 'Celsius' ? 'C' : 'F'}</h2>
         <img src={`https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`} width={60} height={60} />
         <h4>{capitalizedDescription}</h4>
